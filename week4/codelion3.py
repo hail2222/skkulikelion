1f364f928d7aaf852c563a516d8227733f5d9c2f
hail2222 <100838589+hail2222@users.noreply.github.com>
2022-05-06 09:50:05 +0900

    HTML/CSS study week4

---
+++ week4/codelion3.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+city = "Seoul"
+apikey = "6f91f92e8f370b5254d247d386cffe79"
+lang = "kr"
+# units - metric
+api = f"""http://api.openweathermap.org/data/2.5/\
+weather?q={city}&appid={apikey}&lang={lang}&units=metric"""
+
+result = requests.get(api)
+# print(result.text)
+
+data = json.loads(result.text)
+
+
+print(data["name"],"의 날씨입니다.")
+print("날씨는 ",data["weather"][0]["description"],"입니다.")
+print("현재 온도는 ",data["main"]["temp"],"입니다.")
+print("하지만 체감 온도는 ",data["main"]["feels_like"],"입니다.")
+print("최저 기온은 ",data["main"]["temp_min"],"입니다.")
+print("최고 기온은 ",data["main"]["temp_max"],"입니다.")
+print("습도는 ",data["main"]["humidity"],"입니다.")
+print("기압은 ",data["main"]["pressure"],"입니다.")
+print("풍향은 ",data["wind"]["deg"],"입니다.")
+print("풍속은 ",data["wind"]["speed"],"입니다.")
