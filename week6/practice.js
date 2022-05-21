5128ee97dff606ec1405c041011aeb903b936164
hail2222 <100838589+hail2222@users.noreply.github.com>
2022-05-21 17:25:13 +0900

    HTML/CSS study week6

---
+++ week6/practice.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".animal")[2].innerHTML="dog";
+const animals=document.getElementById("animals");
+animals.innerHTML+="<li class ='animal'>Cat</li>";
+document.querySelectorAll(".box")[0].classList.add("purple");
+document.querySelectorAll(".box")[0].classList.remove("purple");
